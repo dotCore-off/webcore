03dd260460f0533ac8d9dfc4dde7d1ba9e7571e0
dotCore <64563384+Real-dotCore@users.noreply.github.com>
2021-02-09 15:49:00 +0100

    Added some notes to code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
+// Blacklisted code
 const BLACKLISTED_KEY_CODES = [38];
+
+// Available commands
 const COMMANDS = {
     help:
         'Commandes disponibles: <span class="code">about</span>, <span class="code">cv</span>, <span class="code">comp</span>',
@@ -8,8 +11,11 @@ const COMMANDS = {
         'Je maîtrise actuellement les éléments suivants :<br><span class="code">Langages ❱</span> HTML / CSS / SCSS / JavaScript / PHP / Lua / C / C++<br><span class="code">Frameworks ❱</span> Symfony / ReactJs / Laravel<br><span class="code">CMS ❱</span> XenForo / IPB / WordPress / WHMCS',
     cv: "Voici la dernière version de mon CV ❱ <a href='./assets/other/CV_BADEL_Alexis_2021_VF.pdf' class='success link'>CV_BADEL_Alexis_2021_VF.pdf</a><br><i>Le numéro de téléphone n'y figure pas par mesure de sécurité.</i>"
 };
+
+// Input vars
 let userInput, terminalOutput;
 
+// Loading core input system
 const app = () => {
     userInput = document.getElementById("userInput");
     terminalOutput = document.getElementById("terminalOutput");
@@ -17,33 +23,49 @@ const app = () => {
     console.log("[Terminal - Webcore] Système prêt !");
 };
 
+// Core function
 const execute = function executeCommand(input) {
+    // Noob var
     let output;
+    
+    // Force to lowercase - Increase compatibility
     input = input.toLowerCase();
+
+    // In case nothing is entered
     if (input.length === 0) {
         return;
     }
+
+    // Output = Style properties + ➜ + User input
     output = `<div class="terminal-line-success"><span class="success">➜</span> ${input}</div>`;
+
+    // In case command doesn't exist yet
     if (!COMMANDS.hasOwnProperty(input)) {
         output += `<div class="terminal-line-error">Commande non-reconnue: <span class="code">${input}</span></div>`;
         console.log("[Terminal - Webcore] Cette commande n'existe pas !");
     } else {
+        // If command exists, we print the predefined output linked to her
         output += COMMANDS[input];
     }
 
+    // Styling of the output
     terminalOutput.innerHTML = `${
         terminalOutput.innerHTML
     }<div class="terminal-line">${output}</div>`;
     terminalOutput.scrollTop = terminalOutput.scrollHeight;
 };
 
+// Key function
 const key = function keyEvent(e) {
+    // Noob var
     const input = userInput.innerHTML;
 
+    // If key is blacklisted
     if (BLACKLISTED_KEY_CODES.includes(e.keyCode)) {
         return;
     }
 
+    // If user press Enter => Execute the program
     if (e.key === "Enter") {
         execute(input);
         userInput.innerHTML = "";
@@ -53,6 +75,7 @@ const key = function keyEvent(e) {
     userInput.innerHTML = input + e.key;
 };
 
+// Delete function
 const backspace = function backSpaceKeyEvent(e) {
     if (e.keyCode !== 8 && e.keyCode !== 46) {
         return;
@@ -63,6 +86,7 @@ const backspace = function backSpaceKeyEvent(e) {
     );
 };
 
+// Setup some listeners
 document.addEventListener("keydown", backspace);
 document.addEventListener("keypress", key);
 document.addEventListener("DOMContentLoaded", app);
